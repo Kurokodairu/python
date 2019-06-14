fd777411350605de1d30fb7be3e4c3707f706e41
Kurokodaiuru <jsvendsli@gmail.com>
2019-06-14 14:50:26 +0200

    idk

--- tweet.py
+++ tweet.py
@@ -5,10 +5,12 @@ auth.set_access_token("1004970613289734144-f3l5vdXPSDIHIo3PWUtJ2BM1Modb86", "KBe
 
 api = tweepy.API(auth)
 
-#user = api.get_user("_Kurokodairu_")
-#print("@_Kurokodairu_ has " + str(user.followers_count) + " Followers.")
+user = api.get_user("_Kurokodairu_")
+print("@_Kurokodairu_ has " + str(user.followers_count) + " Followers.")
 
 search = api.search("Capitalized File Names... :/ ")
 
-print(search[0].text)
 print(search[0].user.name)
+print(search[0].text)
+
+
