1c20755860e1143ed51e5c97b73bbdc2269b6300
Jørgen <39915109+Kurokodairu@users.noreply.github.com>
2023-08-20 22:22:40 +0200

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,7 +1,7 @@
 import tweepy
 
-auth = tweepy.OAuthHandler("QI8vmScHdMpvQSey7KlGjnmyd", "D3xUJ1EONJIZv0Yqun8kYETASQVWCIHvVJpsbTElc941yR5C0A")
-auth.set_access_token("1004970613289734144-f3l5vdXPSDIHIo3PWUtJ2BM1Modb86", "KBetHUImvnOXO012rnNgtcwbpyEhDxv2sJT528DB5HSam")
+auth = tweepy.OAuthHandler("")
+auth.set_access_token("")
 
 api = tweepy.API(auth)
 
